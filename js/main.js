4ecc6292a287276792bcef6c76ccfef268fcea83
Zeheloth <noa.faretra@divcom.ch>
2024-04-17 15:43:12 +0200

    envoie du mdp et confirmation, puis verification

--- js/main.js
+++ js/main.js
@@ -6,3 +6,30 @@
 'use strict';
 
 console.log('Test liaison JS');
+
+//Récupération du formulaire
+const formulaire = document.querySelector('form');
+const txtMDP = document.querySelector('#mdp');
+const txtMDPConfirm = document.getElementById('mdpC');
+console.log(formulaire, txtMDP,txtMDPConfirm);
+
+//Ecouter l'envoie du formulaire
+
+formulaire.addEventListener('submit',function Coucou (event){
+    //sa stop l'envoie du formulaire
+    event.preventDefault();
+
+    //recuper les valeur du mdp et de la confirmation
+    let mdp= txtMDP.value;
+    let mdpConfirm = txtMDPConfirm.value;
+
+    //tester si les mdp son identiques
+    if (mdp !== mdpConfirm) {
+        alert("les mots de passe sont différents ");
+    }
+    else {
+        alert('Comptes crée avec succès !');
+        //envoie le formulaire
+        formulaire.submit();
+    }
+});
